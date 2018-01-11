9f3bea92bbf0727505313a273241b19a4ee3b1bd
es5es5 <es5e5s@nate.com>
2018-01-11 12:39:18 +0900

    Variable

---
+++ Jan11-2_Variable/src/Variable.java
@@ -0,0 +1,13 @@
+
+public class Variable {
+
+	public static void main(String[] args) {
+		int myAge = 26;
+		int phone = 800000;
+		float eye = 1.2f;
+		System.out.println(myAge);
+		System.out.println(phone);
+		System.out.println(eye);
+	}
+
+}
